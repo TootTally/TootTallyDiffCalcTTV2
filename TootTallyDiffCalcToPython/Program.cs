0509bdb137dc37747fd3fc8aed2185fffd8b2902
Electrostats <Super_justy@hotmail.com>
2024-11-06 13:30:51 -0500

    Updated build date, test run

--- TootTallyDiffCalcToPython/Program.cs
+++ TootTallyDiffCalcToPython/Program.cs
@@ -6,7 +6,7 @@ namespace TootTallyDiffCalcTTV2
     {
         public static List<Chart> chartList;
         public const string VERSION_LABEL = "5.1.1";
-        public const string BUILD_DATE = "11052024";
+        public const string BUILD_DATE = "11062024";
         public static StreamWriter fileWriter;
 
         public static void Main()
